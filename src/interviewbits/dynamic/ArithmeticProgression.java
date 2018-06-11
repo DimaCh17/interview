edc3aeab12c16b6a8a6b4f6eeb570b6ce93a931b
Dmitriy Chernyavsky <>
2018-06-11 08:09:49 -0700

    Partial again

--- src/interviewbits/dynamic/ArithmeticProgression.java
+++ src/interviewbits/dynamic/ArithmeticProgression.java
@@ -1,7 +1,6 @@
 package interviewbits.dynamic;
 
 import java.util.*;
-import java.util.function.Predicate;
 
 /*
 Find longest Arithmetic Progression in an integer array and return its length. More formally, find longest sequence of indices, 0 < i1 < i2 < … < ik < ArraySize(0-indexed) such that sequence A[i1], A[i2], …, A[ik] is an Arithmetic Progression. Arithmetic Progression is a sequence in which all the differences between consecutive pairs are the same, i.e sequence B[0], B[1], B[2], …, B[m - 1] of length m is an Arithmetic Progression if and only if B[1] - B[0] == B[2] - B[1] == B[3] - B[2] == … == B[m - 1] - B[m - 2].
@@ -47,47 +46,55 @@ public class ArithmeticProgression {
         System.out.print(String.format(format, args));
     }
 
-    private <T> void printIfDiffers(T was, T now) {
-        if (!was.equals(now)) {
-            print("The value changed from %s to %s", was, now);
-        }
-    }
     private class Sequence {
         public final int start; // index
         public final int step; // step
 
-        private final Deque<Integer> elements = new LinkedList <>();
+        private int dataIndex = 0;
+        private int[] data = new int[2];
         private int count = 0;
 
         public Sequence(int start, int next) {
             this.start = start;
             this.step = next - start;
-            count = 2;
-            elements.addFirst(start);
-            elements.addLast(next);
+            data[0] = start;
+            data[1] = next;
+            dataIndex = 1;
             count = 2;
         }
 
-        // TODO: check if to handle out of sequence elements by ignoring them.
+        @Override
+        public int hashCode() {
+            return Objects.hash(start, step);
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            if (other == this) {
+                return true;
+            }
+            if (other == null) {
+                return false;
+            }
+            if (!(other instanceof Sequence)) {
+                return false;
+            }
+            Sequence sequence = (Sequence) other;
+            return start == sequence.start && step == sequence.step && count == sequence.count;
+        }
+
         public void add(int val) {
-            elements.addLast(val);
-            elements.removeFirst();
+            if (val != next()) {
+                throw new RuntimeException();
+            }
+            data[(dataIndex + 1) % 2] = val;
+            dataIndex = (dataIndex + 1) % data.length;
             count += 1;
         }
 
-        public int safeAdd(int val) {
-            if (val == next()) {
-                add(val);
-            }
-            return count;
-        }
-
-        private int last() {
-            return elements.peekLast();
-        }
 
         public int next() {
-            return last() + step;
+            return data[dataIndex] + step;
         }
 
     }
@@ -96,26 +103,49 @@ public class ArithmeticProgression {
         // we keep track of two last elements per sequence, along with the total count
 
         // check existing sequences if they can be continued
-        Map<Integer, List<Sequence>> sequenceBuckets = new HashMap<>();
+        //Map<Integer, List<Sequence>> sequenceBuckets = new HashMap<>(); // list of sequence hides one *N
         Set<Integer> startSet = new HashSet <>();
+        // holds values for each value that will be be added to a sequence.
+        Map<Integer, Set<Sequence>> anticipationMap = new HashMap<>();
         int maxLen = 0;
         for (int i = 0; i < list.size(); i++) {
             int val = list.get(i);
-            for (int step : sequenceBuckets.keySet()) {
-                for (Sequence sequence : sequenceBuckets.get(step)) {
-                    int wasMax = maxLen;
-                    sequence.safeAdd(val); // LL too complicated
-                    printIfDiffers(wasMax, maxLen);
-                    maxLen = Math.max(maxLen, sequence.count); //
+            // use the anticipation sets here.
+            Set<Integer> stepsUsed = new HashSet <>();// once the value became a part of a sequence of this step
+            // it can't start a sequence with the same step from the start set.
+            Set<Sequence> anticipationSet = anticipationMap.getOrDefault(val, new HashSet <>());
+            List<Sequence> sequencesHit = new LinkedList <>();
+            for (Sequence sequence : anticipationSet) { // (LL) don't modify the collection while iterating it
+
+                sequence.add(val); // if the sequence lands in the same set we don't want to delete it
+                int nextVal = sequence.next();
+
+                Set<Sequence> nextSet = anticipationMap.getOrDefault(nextVal, new HashSet <>());
+                nextSet.add(sequence);
+
+                if (!anticipationSet.equals(nextSet)) {
+                    sequencesHit.add(sequence); // (LL) handle the situation when we hit the same bucket
+                    // in case of the same value, and step == 0
                 }
+
+                // the outcome completely
+                anticipationMap.put(nextVal, nextSet);
+                maxLen = Math.max(maxLen, sequence.count);
+                stepsUsed.add(sequence.step);
             }
+            sequencesHit.forEach(s ->  anticipationSet.remove(s));
+            anticipationMap.put(val, anticipationSet);// remove the sequence from old
+
+
             for (int start : startSet) {
-                int newKey = val - start;
-                Sequence newSequence = new Sequence(start, val);
-                List<Sequence> sequenceList = sequenceBuckets.getOrDefault(newKey, new ArrayList <>());
-                sequenceList.add(newSequence);
-                sequenceBuckets.put(newKey, sequenceList);
-                maxLen = Math.max(maxLen, newSequence.count); //
+                //int step = val - start;
+                Sequence sequence = new Sequence(start, val);
+                if (!stepsUsed.contains(sequence.step)) {
+                    Set <Sequence> nextSet = anticipationMap.getOrDefault(sequence.next(), new HashSet <>());
+                    nextSet.add(sequence);
+                    anticipationMap.put(sequence.next(), nextSet);
+                    maxLen = Math.max(maxLen, sequence.count);
+                }
             }
             startSet.add(val);
             maxLen = Math.max(maxLen, 1);// LL - check CC
--- src/interviewbits/dynamic/ArithmeticProgressionTest.java
+++ src/interviewbits/dynamic/ArithmeticProgressionTest.java
@@ -8,10 +8,26 @@ import static org.junit.Assert.*;
 public class ArithmeticProgressionTest {
 
     @Test
+    public void solve16() {
+        ArithmeticProgression ap = new ArithmeticProgression();
+        ap.showOutput = true;
+        assertEquals(16, ap.solve(ArrayUtils.create(
+    729652, 1382614, 740882302, 688260158, 2035576, 2688538, 879350478, 3341500, 3994462, 4647424, 5300386, 174780165, 902753309, 67221396, 261170013, 5953348, 20981554, 697413800, 634979324, 6606310, 7259272, 7912234, 8565196, 365426960, 9218158, 9871120, 10524082, 214497117, 780770428, 33203066, 530134298, 207525479, 339378323, 108777178, 466377836, 135845030, 338758184, 16429964, 614474430, 366933075, 974577226, 276742682, 124388355, 808614382, 721196653, 221239448, 7325136, 544225976, 564151016, 573481752
+        )));
+    }
+
+    @Test
     public void solve() {
         ArithmeticProgression ap = new ArithmeticProgression();
         ap.showOutput = true;
         assertEquals(3, ap.solve(ArrayUtils.create(9, 4, 7, 2, 10)));
+    }
+
+    @Test
+    public void solve2() {
+        ArithmeticProgression ap = new ArithmeticProgression();
+        ap.showOutput = true;
+        assertEquals(4, ap.solve(ArrayUtils.create(3, 6, 9, 12)));
     }
 
     @Test
@@ -33,5 +49,20 @@ public class ArithmeticProgressionTest {
         ArithmeticProgression ap = new ArithmeticProgression();
         ap.showOutput = true;
         assertEquals(2, ap.solve(ArrayUtils.create(2, 2)));
+    }
+
+
+    @Test
+    public void cc3() {
+        ArithmeticProgression ap = new ArithmeticProgression();
+        ap.showOutput = true;
+        assertEquals(10, ap.solve(ArrayUtils.create(1, 1, 1, 1, 1, 1, 1, 1, 1, 1)));
+    }
+
+    @Test
+    public void cc3_1() {
+        ArithmeticProgression ap = new ArithmeticProgression();
+        ap.showOutput = true;
+        assertEquals(3, ap.solve(ArrayUtils.create(1, 1, 1)));
     }
 }
