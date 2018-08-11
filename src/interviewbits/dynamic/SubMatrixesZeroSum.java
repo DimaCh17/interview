bd7c89a30cc6dce57fa99fbf380d1c789eacad82
Dmitriy Chernyavsky <dimach1976@gmail.com>
2018-08-11 11:36:06 -0700

    Fixed one problem, when used bigArea==0 to find zeros, changed to area==0

--- src/interviewbits/dynamic/SubMatrixesZeroSum.java
+++ src/interviewbits/dynamic/SubMatrixesZeroSum.java
@@ -6,6 +6,72 @@ import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
+/*
+Given a matrix M of size nxm and an integer K, find the maximum element in the K manhattan distance neighbourhood for all elements in nxm matrix.
+In other words, for every element M[i][j] find the maximum element M[p][q] such that abs(i-p)+abs(j-q) <= K.
+
+Note: Expected time complexity is O(N*N*K)
+
+Constraints:
+
+1 <= n <= 300
+1 <= m <= 300
+1 <= K <= 300
+0 <= M[i][j] <= 1000
+Example:
+
+Input:
+M  = [[1,2,4],[4,5,8]] , K = 2
+
+Output:
+ans = [[5,8,8],[8,8,8]]
+Given a matrix M of size nxm and an integer K, find the maximum element in the K manhattan distance neighbourhood for all elements in nxm matrix.
+In other words, for every element M[i][j] find the maximum element M[p][q] such that abs(i-p)+abs(j-q) <= K.
+
+Note: Expected time complexity is O(N*N*K)
+
+Constraints:
+
+1 <= n <= 300
+1 <= m <= 300
+1 <= K <= 300
+0 <= M[i][j] <= 1000
+Example:
+
+Input:
+M  = [[1,2,4],[4,5,8]] , K = 2
+
+Output:
+ans = [[5,8,8],[8,8,8]]
+
+(I1) go from left to right, and keep a count map (tree map) of values to their counts, that are within
+the pattern. once the value goes away
+keep a copy of the matrix to store max, thus they don't interact with the original.
+(A)
+(1) start with 0, 0. get all elements in range (it will be a half pattern + 1), put them into the map.
+(2) move the target left (or down) until the last element is reached, may use a single coordinate
+    but that may be too much now
+(3) remove all elements that are not covered by the previous (direction) pattern
+  based on (direction) passed to the step.
+
+
+(A2). use the dp to check rectanges in a strip
+(1) iterate the cols
+(2) use areas in the strip, when upper rows areas are removed
+    by use prefix sums.
+(3) start from the left
+(4) check if any of cached rectangles in the strip has the same area
+(5) if it does, then the strip segment (a difference between the big and cached)
+    is 0, need to increase res ...
+(6) ?? does each cached rectangle introduce a different result (adds to the crowd)
+(7) what does it mean if there were 2 rectangles in the strip with the same area
+    zeros is a great example.
+IDX:  0  1  2
+INP: [0, 0, 0]
+both [0], and [0, 1] (we cache NOT [1] as it doesn't start on the left)
+it means that [1, 2] and [2] have 0 area.
+*/
+
 public class SubMatrixesZeroSum {
 
 	public int[][] getPrefixSum(ArrayList<ArrayList<Integer>> input) {
@@ -76,7 +142,6 @@ public class SubMatrixesZeroSum {
 					smallArea = pre[r1 - 1][col];
 				}
 				int area = bigArea - smallArea;
-				//print(" cube_res: %3d", cubeRes);
 				if (areaMap.containsKey(area)) {
 					int found = areaMap.get(area);
 					// handle multiple entries with the same area that
@@ -91,8 +156,8 @@ public class SubMatrixesZeroSum {
 				// steps, as only areas for smaller columns are there
 				// which is a DP point
 				//areaMap.put(area, areaMap.getOrDefault(area, 0) + 1);
-				areaMap.put(area, areaMap.getOrDefault(area, 0) + 1);
-				if (bigArea == 0) {
+				putToCache(area, getFromCache(area, areaMap) + 1, areaMap);
+				if (area == 0) { //? do we actually need that or it produces dups?
 					res++;
 				}
 			}
@@ -100,6 +165,14 @@ public class SubMatrixesZeroSum {
 			print("\n");
 		}
 		return res;
+	}
+
+	private void putToCache(int key, int value, Map<Integer, Integer> cache) {
+		cache.put(key, value);
+	}
+	
+	private int getFromCache(int key, Map<Integer, Integer> cache) {
+		return cache.getOrDefault(key, 0);
 	}
 
 	public boolean showOutput = false;
